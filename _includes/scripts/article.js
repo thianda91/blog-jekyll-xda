646be16eee9abd37af8a53ea35e7f92240e6d27c
thianda <thianda91@outlook.com>
2025-05-11 22:27:09 +0800

    Improve page functionality

--- _includes/scripts/article.js
+++ _includes/scripts/article.js
@@ -21,4 +21,9 @@
       });
     });
   });
+  document.querySelectorAll('a').forEach(link => {
+    if (link.hostname !== window.location.hostname) {
+      link.setAttribute('target', '_blank');
+    }
+  });
 })();
